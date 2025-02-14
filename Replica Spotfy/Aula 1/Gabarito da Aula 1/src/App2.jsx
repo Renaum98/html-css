37d5e45aba1d0e44c5613d9e7047d706f7a5550f
Renaum98 <167568624+Renaum98@users.noreply.github.com>
2025-02-14 11:08:02 -0300

    q

---
+++ Replica Spotfy/Aula 1/Gabarito da Aula 1/src/App2.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+const App2 = () => {
+    return <div>App2</div>;
+};
+
+export default App2;
+
+// reface
+// Arrow Function
+// const App2 = () => <h1>Olá, Mundo!</h1>
+// Nomeação de Componentes
+// PascalCase = sempre nomeação com a primeira letra em maiusculo ex: MainComp()
+
+// export default, posso importar com qualquer nome e sem chaves
+// export sem default, so posso importar entre chaves com o nome exportado 

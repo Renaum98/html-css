98c97e0307c985ed91d392d08c6349f39aaefbef
Renaum98 <167568624+Renaum98@users.noreply.github.com>
2025-02-14 10:33:56 -0300

    qwe

--- Replica Spotfy/Aula 1/Gabarito da Aula 1/src/App.jsx
+++ Replica Spotfy/Aula 1/Gabarito da Aula 1/src/App.jsx
@@ -1,9 +1,3 @@
-// Arrow Function = Cria funções de outra forma
-const App2 = () => {
-    console.log('Olá, Mundo!')
-
-    return <h1>Olá, Mundo!</h1>
-};
 
 function App(){
     console.log('Olá, Mundo!')
